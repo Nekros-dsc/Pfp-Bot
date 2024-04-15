5198d1a4c554b9be6f2301fe030942313552c10e
Uhql <122643715+Nekros-dsc@users.noreply.github.com>
2024-04-15 11:53:24 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-class config {
-    constructor() {
-        this.token = "put-token-here"
-        this.time = 10000
-        this.color = "480ca8"
-        this.channels = ["channel-id"]
-    }
-}
-module.exports = new config()
